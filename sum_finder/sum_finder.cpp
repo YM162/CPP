0a6b3339630a955dfbc98ead0c35d6e0fe321cb0
YM162 <78236156+YM162@users.noreply.github.com>
2022-04-12 19:45:35 +0200

    Create sum_finder.cpp

---
+++ sum_finder/sum_finder.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+std::vector <int> whatsum(std::vector <int> lista,int target){
+    std::sort(lista.begin(),lista.end());
+    std::vector <int> whatsum;
+    for (auto i:lista){
+        if (i<=target){
+            for (auto j:lista){
+                if (j>=i && j<=target){
+                    if (j+i==target){whatsum.push_back(i);whatsum.push_back(j);}
+                }
+            }
+        }
+    }
+
+
+
+    
+    return whatsum;
+
+}
+
+int main(){
+    std::vector <int> lista = {3,4,5,2,1,0};
+    int target = 9;
+
+    std::vector <int> sum = whatsum(lista,target);
+    for (auto i:sum){
+        std::cout << i <<" ";
+    }
+
+    return 0;
+}
